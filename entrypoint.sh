be4605458cd4c3d61cf5ebeb83bb6af18ab3ec63
Veovis <veovis@kveer.fr>
2019-03-18 13:05:22 +0100

    fixing syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
         milter)
             echo 'Running in milter mode...'
             set -- /usr/sbin/clamav-milter
+            ;;
         *)
             set -- /bin/sh
             ;;
@@ -32,6 +33,7 @@ sed -i 's/^#\(Foreground \).*/\1yes/' $CLAMAV_ETC/clamd.conf
 sed -i 's/^#\(Foreground \).*/\1yes/' $CLAMAV_ETC/clamav-milter.conf
 sed -i 's/^#\(TCPSocket \)/\1/' $CLAMAV_ETC/clamd.conf
 sed -i 's/^#\(MilterSocket inet:7357\).*/\1/' $CLAMAV_ETC/clamav-milter.conf
+sed -i 's/^#\(ClamdSocket \).*/\1tcp:clamav:3310/' $CLAMAV_ETC/clamav-milter.conf
 
 if [ "$INITIAL_UPDATE" = "1" ] && [ ! "$(ls -A $CLAMAV_VAR_LIB)" ]; then
     echo 'Running first-time freshclam...'
